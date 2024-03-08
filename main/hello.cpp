34ed92c2b70c7aa437a0ff66e30a739b5376f222
Chandana Mulagund <99470852+chandanamulagund@users.noreply.github.com>
2024-03-08 18:41:00 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "This is PES1UG21CS149\n";
+  cout << "Webhook successfully enabled\n";
   return 0;
 }
